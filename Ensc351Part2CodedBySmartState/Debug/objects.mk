a65ea964f0e2c619f8ef15f41bacd1273e14c371
proxy-hatch <wyx_100@hotmail.com>
2017-11-27 16:20:55 -0800

    reverted to contain modified src code only

--- Ensc351Part2CodedBySmartState/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lEnsc351Part2SolnLib -lEnsc351x-myio -lEnsc351xmodem -lEnsc351 -lpthread
-
--- Ensc351Part2HandCoded/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lEnsc351Part2SolnLib -lEnsc351xmodem -lEnsc351x-myio -lEnsc351 -lpthread
-
